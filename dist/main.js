daeb6edfb49a27521e47e4b94af12b6fa7f31dc7
catalinbroinas <catalinbroinas@yahoo.com>
2024-03-07 10:27:57 +0200

    Implement displayHome(): generate home page content  * Add module to home.js for creating and displaying home page content

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,125 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   displayHome: () => (/* binding */ displayHome)
+/* harmony export */ });
+function displayHome() {
+    const home = document.createElement('div');
+    const pageTitle = document.createElement('div');
+    const subTitle = document.createElement('h2');
+    const button = document.createElement('button');
+    const logo = document.createElement('img');
+    const title = document.createElement('h1');
+    const titleSpan = document.createElement('span');
+
+    home.classList.add('home');
+    home.setAttribute('id', 'home');
+    subTitle.classList.add('sub-title');
+    pageTitle.classList.add('page-title');
+    logo.setAttribute('src', './img/odin.png');
+    logo.setAttribute('alt', 'Odin Project');
+    logo.classList.add('logo');
+    title.classList.add('title');
+    button.setAttribute('type', 'button');
+    button.classList.add('btn-primary');
+
+    subTitle.textContent = 'Welcome to our restaurant!';
+    titleSpan.textContent = 'Odin';
+    button.textContent = 'View Menu';
+
+    home.appendChild(subTitle);
+    home.appendChild(pageTitle);
+    pageTitle.appendChild(logo);
+    pageTitle.appendChild(title);
+    title.appendChild(titleSpan);
+    title.insertAdjacentText('beforeend', ' Restaurant');
+    home.appendChild(button);
+
+    return home;
+}
+
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ "./src/home.js");
+
+
+
+})();
 
 /******/ })()
 ;
+//# sourceMappingURL=main.js.map
---
+++ src/home.js
@@ -0,0 +1,36 @@
+function displayHome() {
+    const home = document.createElement('div');
+    const pageTitle = document.createElement('div');
+    const subTitle = document.createElement('h2');
+    const button = document.createElement('button');
+    const logo = document.createElement('img');
+    const title = document.createElement('h1');
+    const titleSpan = document.createElement('span');
+
+    home.classList.add('home');
+    home.setAttribute('id', 'home');
+    subTitle.classList.add('sub-title');
+    pageTitle.classList.add('page-title');
+    logo.setAttribute('src', './img/odin.png');
+    logo.setAttribute('alt', 'Odin Project');
+    logo.classList.add('logo');
+    title.classList.add('title');
+    button.setAttribute('type', 'button');
+    button.classList.add('btn-primary');
+
+    subTitle.textContent = 'Welcome to our restaurant!';
+    titleSpan.textContent = 'Odin';
+    button.textContent = 'View Menu';
+
+    home.appendChild(subTitle);
+    home.appendChild(pageTitle);
+    pageTitle.appendChild(logo);
+    pageTitle.appendChild(title);
+    title.appendChild(titleSpan);
+    title.insertAdjacentText('beforeend', ' Restaurant');
+    home.appendChild(button);
+
+    return home;
+}
+
+export { displayHome };
